0aab986ab06c9e2bc7776bd8672350056627f8a1
Adeonir Kohl <adeonir@gmail.com>
2020-01-17 01:03:48 -0300

    Split into components

---
+++ src/component/UserInfo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const UserInfo = () => (
+  <div className='user-info'>
+    <img src='https://avatars3.githubusercontent.com/u/3428733?v=4' alt='adeonir' />
+    <h1 className='username'>
+      <a href='https://github.com/adeonir'>Adeonir Kohl</a>
+    </h1>
+    <div className='repos-info'>
+      <ul>
+        <li>Repositórios: 35</li>
+        <li>Seguidores: 99</li>
+        <li>Seguindo: 137</li>
+      </ul>
+    </div>
+  </div>
+)
+
+export default UserInfo
